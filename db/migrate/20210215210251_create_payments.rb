e7636c2f09610386878bfde86e24a0cd24aa9b6a
almokhtarbr <almokhtarbekkour@gmail.com>
2021-02-15 22:12:52 +0100

    [x] Removing id from relation many to many between user and course [x] Add Seeding to user, course, payment

--- db/migrate/20210215210251_create_payments.rb
+++ db/migrate/20210215210251_create_payments.rb
@@ -1,9 +1,8 @@
 class CreatePayments < ActiveRecord::Migration[6.1]
   def change
-    create_table :payments do |t|
+    create_table :payments, id: false do |t|
       t.references :user, null: false, foreign_key: true
       t.references :course, null: false, foreign_key: true
-
       t.timestamps
     end
   end
